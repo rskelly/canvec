9660009eabef71ef1c8a8192b3f33dc611b2b9f6
rob <rob@dijital.(none)>
2012-10-20 11:02:09 +0400

    Fixed out-of-order parameter problem. Added the ability for the program to extract and consume zip files in batches.

--- canvec.py
+++ canvec.py
@@ -20,34 +20,259 @@
 from zipfile import ZipFile
 import os
 import re
+import subprocess
+import gzip
+import zlib
 
-class CanvecExtractor:
+class Base(object):
 	"""
-		Provides a method for extracting CanVec files matching a search string 
-		and creating an PostGIS-compatible SQL file to create and populate a table 
-		containing the data.
+	Base class. Provides convenient methods.
 	"""
-	def __init__(self):
-		# The maximum size an sql file is allowed to reach before it gets rotated.
-		self.maxSqlSize = 500 * 1024 * 1024
-		
-	def extract(self, search, tableName, schemaName, sqlFile, canvecDir, tmpDir):
+	
+	def _dirExists(self, folder):
+		"""Returns true if a directory exists. False otherwise."""
+		return os.access(folder, os.R_OK|os.W_OK)
+
+	def _deleteFile(self, filename):
 		"""
-			Perform the extraction and create the SQL file.
+		Deletes a file. Swallows the error if there is one.
+
+		Arguments:
+			file -- The path to the file.close
+		"""
+		try:
+			os.unlink(filename)
+		except:
+			pass
+
+	def _createDir(self, folder):
+		"""
+		Create a directory. Will attempt to create any parent
+		directories.
+
+		Arguments:
+			folder -- The name of the directory.
+		"""
+		os.makedirs(folder)
+
+
+class ShapefileList(Base):
+	"""
+	Provides a means of iterating over a list of shapefiles contained in a
+	heirarchical list of zip files. Extracts shapefiles in batches of configurable
+	size, matching the provided pattern. And disposes of each temporary file
+	once it has been passed.
+	"""
+	
+	def __init__(self, pattern, folder, tmpDir = '/tmp', batchSize = 50):
+		"""
+		Initializes a ShapefileList object.
+		
+		Arguments:
+			pattern -- A regular expression matching the canvec feature code of a shapefile.
+			folder -- The data directory where the archives are stored.
+			tmpDir -- The temporary directory where archives are extracted.
+			batchSize -- The number of shapefiles to be extracted at a time. This many are extracted, and they're deleted as the list is consumed.
+		"""
+		if not pattern:
+			raise Exception('The pattern parameter is required.')
+		if not folder or not self._dirExists(folder):
+			raise Exception('The data directory, {0}, does not exist or is not accessible.'.format(folder))
+		if not tmpDir or not self._dirExists(tmpDir):
+			raise Exception('The temporary directory, {0}, does not exist or is not accessible.'.format(tmpDir))
+		self.pattern = pattern
+		self.folder = folder
+		self.tmpDir = tmpDir
+		self.batchSize = batchSize
+		self.shpList = None
+		self.zipList = None
+		self._currentShp = None
+		self._triggerCleanup = False
+		
+	def _loadZipList(self):
+		"""
+		Returns a list of all the zip archives under the given directory.
+		Search is recursive. If no archive is found, the empty list is returned.
+		"""
+		fileList = list()
+		# A regex for finding the zip files.
+		match = re.compile('\.zip$')
+		# Get a list of all files under the canvec directory.
+		files = os.walk(self.folder)
+		# Iterate over the file list and add the zip files to the 
+		# file list. It doesn't really matter if we pick up non-canvec zips, 
+		# because we're only going to extract files that match the search
+		# string.
+		for f in files:
+			dirpath = f[0]
+			for l in f[2]:
+				if match.search(l):
+					fileList.append("{0}/{1}".format(dirpath, l))
+		self.zipList = fileList
+		self.zipIndex = 0
+		
+	def _loadShpList(self):
+		"""
+		Loads (extracts) a batch of shapefiles from an archive. Only those that match
+		the given pattern are extracted.
+		"""
+		shpFiles = list()
+		tmpFiles = list()
+		shpmatch = re.compile('\.shp$')
+		searchRe = re.compile(self.pattern)
+		# Iterate through the list of archives.
+		for i in range(self.zipIndex, len(self.zipList)):
+			f = self.zipList[i]
+			try:
+				# Open the zip file and read the entries.
+				z = ZipFile(f)
+				entries = z.namelist()
+				# Iterate through the entries. If an entry matches the search string, extract
+				# it to the tmp folder.
+				for e in entries:
+					if searchRe.search(e):
+						z.extract(e, self.tmpDir)
+						# Add to the list of extracted files.
+						tmpFiles.append(e)
+						# If the entry is a shapefile, add it to the shp list.
+						if shpmatch.search(e):
+							shpFiles.append(e)
+			except Exception, e:
+				print "Failed to open {0}: {1}.".format(f, e.__str__())
+			finally:
+				try:
+					z.close()
+				except:
+					pass
+			# Set the current zip index for next batch load.
+			self.zipIndex = i + 1
+			# If we have enough shapefiles, stop.
+			if len(shpFiles) >= self.batchSize:
+				break
+		# Save the list of shapefiles.
+		self.shpList = shpFiles
+		self.tmpFiles = tmpFiles
+
+	def next(self):
+		"""
+		Returns the next shapefile in the list.
+		"""
+		if self._triggerCleanup:
+			try:
+				for f in self._cleanupFiles:
+					self._deleteFile(os.path.join(self.tmpDir, f))
+			except:
+				pass
+			self._triggerCleanup = False			
+		if self.shpList and len(self.shpList) == 1:
+			self._triggerCleanup = True
+			self._cleanupFiles = self.tmpFiles
+		if not self.hasNext():
+			return False
+		# Get the next shapefile name.
+		self._currentShp = self.shpList.pop(0)
+		# Return the entire path.
+		return self._currentShp
+
+	def hasNext(self):
+		"""
+		Returns true if there's at least one more shapefile to come.
+		"""
+		if not self.zipList:
+			self._loadZipList()
+		if not self.shpList or len(self.shpList) == 0:
+			self._loadShpList()
+		if not self.shpList or len(self.shpList) == 0:
+			return False
+		return True
+
+	def cleanup(self):
+		"""
+		Cleans up the extracted files.
+		"""
+		try:
+			for f in self.tmpFiles:
+				self._deleteFile(os.path.join(self.tmpDir, f))
+		except:
+			pass
+		self.zipList = None
+		self.shpList = None
+
+	def __del__(self):
+		self.cleanup()
+		
+class ZipWriter(object):
+	
+	def __init__(self, filename):
+		self._h = gzip.open(filename, "wb")
+		
+	def flush(self):
+		self._h.flush()
+		
+	def seek(self, offset, whence = 0):
+		self._h.seek(offset, whence)
+		
+	def close(self):
+		self._h.close()
+		
+	def read(self, size):
+		return zlib.decompress(self._h.read(size))
+	
+	def write(self, value):
+		value = zlib.compress(value, 9)
+		self._h.write(value)
+		
+	def tell(self):
+		self._h.tell()
+		
+	def truncate(self, size):
+		self._h.truncate(size)
+		
+	def writelines(self, lines):
+		for l in lines:
+			self._h.write(zlib.compress(l))
 			
-			Arguments:
-				search     -- A search string to match the name of the file (the file's data is identifiable by its name. For example, metric contours would match 'FO_1030009').
-				tableName  -- The name of the table to create/populate. The table will be dropped!
-				schemaName -- The schema name of the table.
-				sqlFile    -- A file to write the SQL to.
-				canvecDir  -- The name of the Canvec directory. Required.
-				tmpDir     -- The temporary directory. If the directory does not exist, it will be created.
+	def __del__(self):
+		try:
+			self._h.close();
+		except:
+			pass
+		
+	@property
+	def closed(self):
+		return self._h.closed
+	
+	def fileno(self):
+		return self._h.fileno()
+	
+class CanvecExtractor(Base):
+	"""
+	Provides a method for extracting CanVec files matching a search string 
+	and creating an PostGIS-compatible SQL file to create and populate a table 
+	containing the data.
+	"""
+
+	def __init__(self):
+		self.encoding = 'utf-8'
+		self.srid = 4326
+		
+	def extract(self, pattern, outFile, canvecDir, tableName, schemaName = 'public', tmpDir = '/tmp'):
+		"""
+		Perform the extraction and create the SQL file.
+		
+		Arguments:
+			pattern     -- A search string to match the name of the file (the file's data is identifiable by its name. For example, metric contours would match 'FO_1030009').
+			outFile    -- A file to write the SQL to.
+			canvecDir  -- The name of the Canvec directory. Required.
+			tableName  -- The name of the table to create/populate. The table will be dropped!
+			schemaName -- The schema name of the table. Defaults to 'public'
+			tmpDir     -- The temporary directory. If the directory does not exist, it will be created. Defaults to '/tmp'
 		"""
 		# Check parameters
-		if sqlFile is None:
-			raise Exception("No SQL file.")
-		if search is None:
-			raise Exception("No search string.")
+		if outFile is None:
+			raise Exception("No output file.")
+		if pattern is None:
+			raise Exception("No pattern string.")
 		if schemaName is None:
 			raise Exception("No schema name.")
 		if tableName is None:
@@ -62,152 +287,92 @@ class CanvecExtractor:
 		# Create the tmp dir if it doesn't exist.
 		if not self._dirExists(tmpDir):
 			self._createDir(tmpDir)
-		self.searchRe = re.compile(search)
+
+		self._shapeFileList = ShapefileList(pattern, canvecDir, tmpDir)
+
+		self.outFile = outFile
 		self.schemaName = schemaName
 		self.tableName = tableName
-		self.canvecDir = canvecDir
 		self.tmpDir = tmpDir
-		self.tmpFiles = []
-		self.sqlFile = sqlFile
-		# Start!
-		archives = self._getArchives()
-		shapefiles = self._extractShapefiles(archives)
-		self._createSql(shapefiles)
-		self._deleteTmpFiles()
 		
-	def _dirExists(self, dir):
-		"""Returns true if a directory exists. False otherwise."""
-		return os.access(dir, os.R_OK|os.W_OK)
+		self._createSql()
 		
-	def _createDir(self, dir):
-		"""Create a directory."""
-		os.makedirs(dir)
-
-	def _extractShapefiles(self, fileList):
+	def _createSql(self):
 		"""
-			Extracts the shapefiles matching the given re from the archives stored in the file list.
-			Returns a list of the shapefiles (shp) in the archive.
-		"""
-		shpfiles = list()
-		shpmatch = re.compile('\.shp$')
-		# Iterate through the list of archives.
-		for f in fileList:
-			try:
-				# Open the zip file and read the entries.
-				z = ZipFile(f)
-				entries = z.namelist()
-				# Iterate through the entries. If an entry matches the search string, extract
-				# it to the tmp folder.
-				for e in entries:
-					if self.searchRe.search(e):
-						z.extract(e, self.tmpDir)
-						# Add to the list of extracted files.
-						self.tmpFiles.append(e)
-						# If the entry is a shapefile, add it to the shp list.
-						if shpmatch.search(e):
-							shpfiles.append(e)
-			except:
-				print "Failed to open {0}.".format(f)
-			finally:
-				try:
-					z.close()
-				except:
-					pass
-		return shpfiles
-	
-	def _deleteTmpFiles(self):
-		"""
-			Deletes the temporary files extracted from the archives.
-		"""
-		for f in self.tmpFiles:
-			try:
-				os.remove("{0}/{1}".format(self.tmpDir, f))
-			except:
-				pass
-		try:
-			os.rmdir(self.tmpDir)
-		except:
-			pass
-		
-	@property
-	def maxSqlSize(self):
-		"""The maximum size (bytes) of generated sql files. If a file's
-		size exceeds this value, it will be rotated."""
-		return self._maxSqlSize
-		
-	@maxSqlSize.setter
-	def maxSqlSize(self, value):
-		minsize = 50 * 1024 * 1024
-		try:
-			value = int(value)
-			if value < minsize:
-				value = minsize
-			self._maxSqlSize = minsize
-		except:
-			self._maxSqlSize = minsize
-
-	def _createSql(self, shapefiles):
-		"""
-			Prints the SQL containing the DDL and data from all the shape files in the given list.
-			This could be huge.
-		"""
-		cmd = "shp2pgsql -s 4326 {6} -I {0}/{1} {2}.{3} {5} {4}"
-		# Split the filename so we can insert a file number. If there's no extension, we're going to add one.
-		sqlName  = self.sqlFile
-		sqlExt = 'sql'
-		if sqlName.rfind('.') > -1:
-			sqlName, sqlExt = sqlName.rsplit('.')
+		Prints the SQL containing the DDL and data from all the shape files in the given list.
+		This could be huge.
+		"""		
+		outName = self.outFile[0:self.outFile.rfind('.')]
+		outExt = self.outFile[self.outFile.rfind('.') + 1:]
 		fileNum = 1
+		maxSqlSize = 100 * 1024 * 1024
+		end = False
 		# If true, use the append operator, otherwise clobber
 		append = False
 		# If true, use the drop/create switch, otherwise append.
 		create = True
-		maxSqlSize = self.maxSqlSize
 		# Iterate over the list of shapefiles, calling shp2pgsql on each one. This outputs the sql.
-		for i in range(0, len(shapefiles)):
-			sqlFile = "{0}.{1}.{2}".format(sqlName, fileNum, sqlExt)
-			os.system(cmd.format(self.tmpDir, shapefiles[i], self.schemaName, self.tableName, sqlFile, '>>' if append else '>', '-d' if create else '-a'))
+		while True:
+			shapefile = self._shapeFileList.next()
+			if not shapefile:
+				print "Done"
+				return
+			# If this is the last file, add the index operator onto the command.
+			if not self._shapeFileList.hasNext():
+				end = True
+			# Create the sql file.
+			outFile = "{0}.{1}.{2}".format(outName, fileNum, outExt)
+			if not append:
+				if outExt == 'gz':
+					raise Exception('Zipping doesn\'t work right now.')
+					h = gzip.open(outFile, 'wb')
+				else:
+					h = open(outFile, 'wb')
+			# First part of command.
+			args = ["shp2pgsql"]
+			# Add the srid
+			args.append("-s {0}".format(self.srid))
+			# Add the create/append switch.
+			if create:
+				args.append("-d")
+			else:
+				args.append("-a")
+			# Add the index switch; only happens at the end.
+			if end:
+				args.append("-I")
+			# Add the encoding
+			args.append('-W {0}'.format(self.encoding))
+			# Add the shapefile name.
+			args.append(shapefile)
+			# Add the schema/table names.
+			args.append("{0}.{1}".format(self.schemaName, self.tableName))
+			# Call the command to output the file.
+			curDir = os.path.abspath(os.curdir)
+			os.chdir(self.tmpDir);
+			p = subprocess.Popen(args, stdout=h)
+			p.wait()
+			os.chdir(curDir);
 			# If the sql file is too large, rotate it.
-			if os.path.getsize(sqlFile) > maxSqlSize:
+			if os.path.getsize(outFile) > maxSqlSize:
+				h.close()
 				fileNum = fileNum + 1
 				append = False
 			else:
 				append = True
+			# Only call with the drop switch on the first iteration.
 			create = False
-			
-	def _getArchives(self):
-		"""
-			Returns a list of all the zip archives under the given directory.
-			Search is recursive. If no archive is found, the empty list is returned.
-		"""
-		fileList = list()
-		# A regex for finding the zip files.
-		match = re.compile('\.zip$')
-		# Get a list of all files under the canvec directory.
-		files = os.walk(self.canvecDir)
-		# Iterate over the file list and add the zip files to the 
-		# file list. It doesn't really matter if we pick up non-canvec zips, 
-		# because we're only going to extract files that match the search
-		# string.
-		for f in files:
-			dirpath = f[0]
-			for l in f[2]:
-				if match.search(l):
-					fileList.append("{0}/{1}".format(dirpath, l))
-		return fileList
-		
 		
 if __name__ == "__main__":
 	import sys
 	args = sys.argv[1:]
-	if len(args) < 6:
-		print "Usage: python canvec.py <search> <schemaname> <tablename> <canvecdir> <tmpdir>\n"
-		print " search     - A search term to use against the shapefile names."
-		print " schemaname - The name of the database schema."
-		print " tablename  - The name of the database table."
-		print " sqlfile    - A file to write the SQL to."
+	if len(args) < 4:
+		print "Usage: python canvec.py <pattern> <outfile> <canvecdir> <tablename> [schemaname] [tmpdir]\n"
+		print " pattern     - A pattern term to use against the shapefile names."
+		print " outfile    - A file to write the SQL to. If it ends with .gz, it's a gzipped file."
 		print " canvecdir  - The directory where the canvec archives are located."
-		print " tmpdir     - A temporary directory for extracting the canvec files."
+		print " tablename  - The name of the database table."
+		print " schemaname - The name of the database schema. Defaults to 'public'"
+		print " tmpdir     - A temporary directory for extracting the canvec files. Defaults to '/tmp'"
 	else:
 		c = CanvecExtractor()
-		c.extract(args[0], args[1], args[2], args[3], args[4], args[5])
+		c.extract(*args)
